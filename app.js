e7ec948d397a3efc80b145a26c000e42ac5abbe7
advert31337 <advert31337@yandex.ru>
2020-05-31 11:34:02 +0300

    prepare to deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParcer = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 const authRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/category')
@@ -39,6 +40,18 @@ app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
 app.use('/api/analytics', analyticsRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist/client'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname, 'client', 'dist', 'client', 'index.html'
+      )
+    )
+  })
+}
 
 module.exports = app
 
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://mdb:111@cluster0-6nshx.mongodb.net/test?retryWrites=true&w=majority',
-  jwt: 'text-jwt_x*'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
+
